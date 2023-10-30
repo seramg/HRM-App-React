3344c38997c04587cbbb3ad805beb4a7a1ec1029
Sera <sera.george@qburst.com>
2023-10-30 16:53:56 +0530

    [add]:basic folder structure

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import "./header.ts"
+function Header() {
+    return <header className="header">
+        <div className="header-content global-width">
+            <a href="javascript:void(0)" className="logo-wrap">
+                <h1 className="page-title">HRM</h1>
+            </a>
+        </div>
+    </header>
+}
+export default Header;
