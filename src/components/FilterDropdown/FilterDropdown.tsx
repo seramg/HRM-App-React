4d933f6f9d4479de30edda408ec3d54115c1a3ce
Sera Mariam George <leelasera19@gmail.com>
2023-11-07 02:37:27 +0530

    [add]:dynamic isMulti for selectdropdown

--- src/components/FilterDropdown/FilterDropdown.tsx
+++ src/components/FilterDropdown/FilterDropdown.tsx
@@ -1,3 +1,4 @@
+import Button from "../Button/Button.tsx";
 import SelectDropDown from "../Select/Select.tsx";
 import FilterDropdownWrapper from "./filterDropdown.ts";
 
@@ -5,7 +6,18 @@ function FilterDropdown() {
   return (
     <FilterDropdownWrapper>
       <h2 className=" head-row dropdown-row">Filter List</h2>
-      <SelectDropDown isMultiState={{ isDepartmentsMulti: true, isDesignationsMulti: true, isEmpModesMulti: true, isSkillsMulti: true }} />
+      <SelectDropDown
+        isMultiState={{
+          isDepartmentsMulti: true,
+          isDesignationsMulti: true,
+          isEmpModesMulti: true,
+          isSkillsMulti: true,
+        }}
+      />
+      <div className="button-container">
+        <Button icon="">Reset</Button>
+        <Button icon="">Save</Button>
+      </div>
     </FilterDropdownWrapper>
   );
 }
--- src/components/FilterDropdown/filterDropdown.ts
+++ src/components/FilterDropdown/filterDropdown.ts
@@ -10,12 +10,19 @@ const FilterDropdownWrapper = styled.section`
   border-radius: 10px;
   min-width: 520px;
 
-  label {
-    padding: 16px;
+  label{
+    margin:0;
+    padding:10px
   }
   .dropdown-row{
     border-bottom: 1px solid var(--secondary-color);
     padding: 10px;
+  }
+  .button-container{
+    display:flex;
+    justify-content: flex-end;
+    gap:10px;
+    padding:10px
   }
 `;
 export default FilterDropdownWrapper;
