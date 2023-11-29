4b4b0a236db4be8e053690548376a269bb0a7ea2
Sera Mariam George <leelasera19@gmail.com>
2023-11-29 18:38:55 +0530

    Moved sensitive information to env files

--- src/components/firebaseConfig.ts
+++ src/components/firebaseConfig.ts
@@ -1,21 +1,11 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getDatabase, } from "firebase/database";
-
 // Web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyBmgesvRfGfX4MF5r4xcTPhNo1sgY3l27U",
-    authDomain: "human-resources-manageme-367c7.firebaseapp.com",
-    databaseURL: "https://human-resources-manageme-367c7-default-rtdb.firebaseio.com",
-    projectId: "human-resources-manageme-367c7",
-    storageBucket: "human-resources-manageme-367c7.appspot.com",
-    messagingSenderId: "40040684447",
-    appId: "1:40040684447:web:5ea1a2a1f6393ad689bbaa",
-    measurementId: "G-D95JZCKQ90"
+export const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_VITE_MEASUREMENT_ID,
 };
-
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-
-// Initialize Realtime Database and get a reference to the service
-export const database = getDatabase(app);
