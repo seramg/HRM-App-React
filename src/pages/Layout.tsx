94537011f224421fe1c064fc8d24af45a12f809f
Sera Mariam George <leelasera19@gmail.com>
2023-12-01 10:29:28 +0530

    [remove]: unnecessary import of react

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header/Header.tsx";
 import Footer from "../components/Footer/Footer.tsx";
-import React from "react";
 
 function Layout() {
   return (
